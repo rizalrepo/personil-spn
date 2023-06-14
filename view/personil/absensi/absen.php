a2f11372272674060dbb47cf9884d11312679a84
rizalrepo <rizalyusei@gmail.com>
2023-06-15 00:25:09 +0800

    ok

--- view/personil/absensi/absen.php
+++ view/personil/absensi/absen.php
@@ -123,35 +123,7 @@
         }
 
         function lokasi_gagal() {
-
-        }
-
-        function latlondist(lat1, lon1, lat2, lon2) {
-            var dlat = deg2rad(lat2 - lat1); // Convierte de grados a radianes.
-            var dlon = deg2rad(lon2 - lon1); // Convierte de grados a radianes.
-            var a = Math.sin(dlat / 2) * Math.sin(dlat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dlon / 2) * Math.sin(dlon / 2);
-            var b = 2 * Math.asin(Math.sqrt(a));
-            var c = (6371 * b * 1000).toFixed(2); // Distancia en km con dos decimales de precisión.
-            return c;
-        }
-
-        function deg2rad(deg) {
-            return deg * 0.017453292519943295; // deg * Math.PI / 180
-        }
-
-        if ("geolocation" in navigator) { //check geolocation available 
-            //try to get user current location using getCurrentPosition() method
-            navigator.geolocation.getCurrentPosition(function(position) {
-                console.log(latlondist(position.coords.latitude, position.coords.longitude, -3.3418973, 114.6069257));
-
-                if (latlondist(position.coords.latitude, position.coords.longitude, -3.3418973, 114.6069257) <= 50) {
-                    alert('OK')
-                } else {
-                    alert('Kamu berada diluar Radius Lokasi Absensi')
-                }
-            });
-        } else {
-            console.log("Browser doesn't support geolocation!");
+            alert("Browser doesn't support geolocation!");
         }
     </script>
 </body>
